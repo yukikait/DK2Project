eaeacdcba315fe17f6b220e3d2aeeca4e717170c
yukikait <yukikait@naver.com>
2019-10-03 09:27:32 +0900

    첫커밋?

---
+++ springbootPorject/src/main/java/youtube/dk2/springbootPorject/SpringbootPorjectApplication.java
@@ -0,0 +1,13 @@
+package youtube.dk2.springbootPorject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootPorjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootPorjectApplication.class, args);
+	}
+
+}
