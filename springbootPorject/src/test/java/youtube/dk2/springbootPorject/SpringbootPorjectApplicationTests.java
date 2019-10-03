7acf8ef9036b9b7fd439db43d3c71fe73adb1edb
yukikait <yukikait@naver.com>
2019-10-03 12:50:56 +0900

    test

--- springbootPorject/src/test/java/youtube/dk2/springbootPorject/SpringbootPorjectApplicationTests.java
+++ springbootPorject/src/test/java/youtube/dk2/springbootPorject/SpringbootPorjectApplicationTests.java
@@ -1,5 +1,7 @@
 package youtube.dk2.springbootPorject;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,6 +13,10 @@ public class SpringbootPorjectApplicationTests {
 
 	@Test
 	public void contextLoads() {
+		
+		
+		assertEquals("test", "test");
+		
 	}
 
 }
